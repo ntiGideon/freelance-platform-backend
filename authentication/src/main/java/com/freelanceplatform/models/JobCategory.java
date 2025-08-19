29cad6de6bd0fb7a74a8f88d9b5c01226ff079b7
Emmanuel Odoi Odotei <99666388+emmanuel-odotei@users.noreply.github.com>
2025-08-19 17:22:55 +0000

    Update JobCategory.java

--- authentication/src/main/java/com/freelanceplatform/models/JobCategory.java
+++ authentication/src/main/java/com/freelanceplatform/models/JobCategory.java
@@ -8,21 +8,18 @@ public class JobCategory {
     
     private String categoryId;
     private String name;
-    
+    private String snsTopicArn;
+
     @DynamoDbPartitionKey
-    public String getCategoryId () {
-        return categoryId;
-    }
+    public String getCategoryId () { return categoryId; }
     
-    public void setCategoryId (String categoryId) {
-        this.categoryId = categoryId;
-    }
+    public void setCategoryId (String categoryId) {this.categoryId = categoryId; }
     
-    public String getName () {
-        return name;
-    }
+    public String getName () { return name; }
     
-    public void setName (String name) {
-        this.name = name;
-    }
+    public void setName (String name) { this.name = name; }
+
+    public String getSnsTopicArn() { return snsTopicArn; }
+    
+    public void setSnsTopicArn(String snsTopicArn) { this.snsTopicArn = snsTopicArn; }
 }
