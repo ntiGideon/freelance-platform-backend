744cb12b741579491a857e22d13f160f8a1edc47
elitekaycy <dicksonanyaele1234@gmail.com>
2025-08-26 08:54:11 +0000

    fix job handler

--- jobs/src/main/java/com/freelance/jobs/owners/ListJobsHandler.java
+++ jobs/src/main/java/com/freelance/jobs/owners/ListJobsHandler.java
@@ -150,7 +150,7 @@ public class ListJobsHandler implements RequestHandler<APIGatewayProxyRequestEve
         return jobs.stream()
                 .filter(job -> categoryId == null || job.categoryId().equals(categoryId))
                 .filter(job -> query == null || matchesSearchQuery(job, query))
-                .toList();
+                .collect(java.util.stream.Collectors.toList());
     }
 
     /**
